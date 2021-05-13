a259e9fa97ec871ec382e274efa90a250e218480
mandych1996 <65146534+mandych1996@users.noreply.github.com>
2021-05-12 21:29:14 -0400

    modification

--- Project1/cost.cpp
+++ Project1/cost.cpp
@@ -10,17 +10,10 @@
 using std::string;
 using std::vector;
 
-/**
- * TODO: change weights for cost functions.
- */
+
 const float REACH_GOAL = pow(10, 6);
 const float EFFICIENCY = pow(10, 5);
 
-// Here we have provided two possible suggestions for cost functions, but feel 
-//   free to use your own! The weighted cost over all cost functions is computed
-//   in calculate_cost. The data from get_helper_data will be very useful in 
-//   your implementation of the cost functions below. Please see get_helper_data
-//   for details on how the helper data is computed.
 
 float goal_distance_cost(const Vehicle &vehicle,
 	const vector<Vehicle> &trajectory,
